6fafc03cd536108d810f5920359db8b576892c80
Little Apps <nick@little-apps.org>
2014-05-13 00:11:15 -0600

    removed unneeded xaml from main.xaml and started on duplicate finder

---
+++ Little System Cleaner/Duplicate Finder/Controls/Start.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Little_System_Cleaner.Duplicate_Finder.Controls
+{
+    /// <summary>
+    /// Interaction logic for Start.xaml
+    /// </summary>
+    public partial class Start : UserControl
+    {
+        private readonly Wizard scanBase;
+
+        public Start(Wizard sb)
+        {
+            InitializeComponent();
+
+            this.scanBase = sb;
+        }
+    }
+}
---
+++ Little System Cleaner/Duplicate Finder/Controls/Wizard.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Little_System_Cleaner.Duplicate_Finder.Controls
+{
+    public class Wizard : UserControl
+    {
+        List<Type> arrayControls = new List<Type>();
+        int currentControl = 0;
+
+        public UserControl userControl
+        {
+            get { return (UserControl)this.Content; }
+        }
+
+        public Wizard()
+        {
+            this.arrayControls.Add(typeof(Start));
+        }
+
+        public void OnLoaded()
+        {
+            this.SetCurrentControl(0);
+        }
+
+        public bool OnUnloaded()
+        {
+
+            return true;
+        }
+
+        /// <summary>
+        /// Changes the current control
+        /// </summary>
+        /// <param name="index">Index of control in list</param>
+        private void SetCurrentControl(int index)
+        {
+            if (this.Dispatcher.Thread != System.Threading.Thread.CurrentThread)
+            {
+                this.Dispatcher.Invoke(new Action(() => SetCurrentControl(index)));
+                return;
+            }
+
+            if (this.userControl != null)
+                this.userControl.RaiseEvent(new RoutedEventArgs(UserControl.UnloadedEvent, this.userControl));
+
+            System.Reflection.ConstructorInfo constructorInfo = this.arrayControls[index].GetConstructor(new Type[] { typeof(Wizard) });
+
+            this.Content = constructorInfo.Invoke(new object[] { this });
+        }
+
+        /// <summary>
+        /// Moves to the next control
+        /// </summary>
+        public void MoveNext()
+        {
+            SetCurrentControl(++currentControl);
+        }
+
+        /// <summary>
+        /// Moves to the previous control
+        /// </summary>
+        public void MovePrev()
+        {
+            SetCurrentControl(--currentControl);
+        }
+
+        /// <summary>
+        /// Moves to the first control
+        /// </summary>
+        public void MoveFirst()
+        {
+            currentControl = 0;
+
+            SetCurrentControl(currentControl);
+        }
+    }
+}
