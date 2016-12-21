fa942b365fa472cf6697c24bbe3af623bb69da4a
Nick H <nick@little-apps.com>
2016-12-21 16:48:52 -0700

    Removed privacy cleaner

--- Little System Cleaner/Privacy Cleaner/Helpers/DetailItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Little_System_Cleaner.Privacy_Cleaner.Helpers
-{
-    public class DetailItem
-    {
-        public string Name { get; set; }
-        public string Size { get; set; }
-        public string AccessDate { get; set; }
-    }
-}
